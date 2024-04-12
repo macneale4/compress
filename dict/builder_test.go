9d43283defce47653b664d1540eed322e24c08e5
Neil Macneale IV <neil@dolthub.com>
2024-04-12 12:35:35 -0700

    Test improvements given by Klaus

--- dict/builder_test.go
+++ dict/builder_test.go
@@ -2,7 +2,7 @@ package dict
 
 import (
 	"bytes"
-	"errors"
+	"io"
 	"math/rand"
 	"os"
 	"testing"
@@ -11,16 +11,28 @@ import (
 )
 
 func TestZStdDict(t *testing.T) {
+	for _, level := range []zstd.EncoderLevel{zstd.SpeedFastest, zstd.SpeedDefault, zstd.SpeedBetterCompression, zstd.SpeedBestCompression} {
+		testZStdDict(t, level)
+	}
+}
+
+func testZStdDict(t *testing.T, level zstd.EncoderLevel) {
+	out := io.Discard
+	if testing.Verbose() {
+		out = os.Stdout
+	}
+
 	opts := Options{
 		MaxDictSize:    2048,
 		HashBytes:      4,
-		Output:         os.Stdout,
+		Output:         out,
 		ZstdDictID:     0,
 		ZstdDictCompat: false,
-		ZstdLevel:      zstd.SpeedBestCompression,
+		ZstdLevel:      level,
 	}
 
-	buff := make([]byte, 0, 4096)
+	inBuf := make([]byte, 0, 4096)
+	outBuf := make([]byte, 0, 4096)
 
 	// This is 32K worth of data, but it's all very similar. Only fits in 4K if compressed with a dictionary.
 	samples := generateSimilarByteSlices(42, 32)
@@ -35,14 +47,14 @@ func TestZStdDict(t *testing.T) {
 
 	totalSize := 0
 	for _, blob := range samples {
-		compressed, err := zCompressDict(buff, dict, blob)
+		compressed, err := zCompressDict(inBuf, dict, blob)
 		if err != nil {
 			t.Fatal(err.Error())
 		}
 		totalSize += len(compressed)
 
 		// Check round trip.
-		decompressed, err := zDecompressDict(buff, dict, compressed)
+		decompressed, err := zDecompressDict(outBuf, dict, compressed)
 		if err != nil {
 			t.Fatal(err.Error())
 		}
@@ -54,40 +66,26 @@ func TestZStdDict(t *testing.T) {
 	if totalSize > 4096 {
 		t.Fatal("Total compressed size exceeds 4096 bytes")
 	}
+	t.Log("Total compressed size:", totalSize)
 }
 
 func zCompressDict(dst, dict, data []byte) ([]byte, error) {
-	if dst == nil {
-		return nil, errors.New("nil destination buffer")
-	}
-
 	encoder, err := zstd.NewWriter(nil, zstd.WithEncoderDict(dict))
 	if err != nil {
 		return nil, err
 	}
 	defer encoder.Close()
 
-	result := encoder.EncodeAll(data, dst) // oddly no error returned here
+	result := encoder.EncodeAll(data, dst[:0])
 	return result, nil
-
 }
 
 func zDecompressDict(dst, dict, data []byte) ([]byte, error) {
-	if dst == nil {
-		return nil, errors.New("nil destination buffer")
-	}
-
 	decoder, err := zstd.NewReader(nil, zstd.WithDecoderDicts(dict))
 	if err != nil {
 		return nil, err
 	}
 	defer decoder.Close()
-
-	// Not sure if Reset is necessary, but just in case.
-	err = decoder.Reset(nil)
-	if err != nil {
-		return nil, err
-	}
 
 	result, err := decoder.DecodeAll(data, dst)
 	if err != nil {
